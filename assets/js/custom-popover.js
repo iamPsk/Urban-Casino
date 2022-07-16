51201ebe16e235eb33e0b83637d1c27999839b2c
webhero <khonjelwayo@gmail.com>
2022-07-16 09:49:38 +0200

    popover

---
+++ assets/js/custom-popover.js
@@ -0,0 +1,9 @@
+
+// When the user clicks on <div>, open the popup
+function myFunction() {
+    console.log('enter');
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+
+//   console.log(e);
+}
